66b9a6b1d19e6d65e31c73b2b5e4cad22849e53b
PabloPicas0 <pawel.fe@gmail.com>
2023-04-05 00:09:16 +0200

    Configured node server

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Your app is listening to localhost:${listener.address().port}`);
+    console.log("Press ctrl + c to exit")
+})
