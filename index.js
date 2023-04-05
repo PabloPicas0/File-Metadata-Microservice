8826056899eff35e6492831a73dabc23384f9327
PabloPicas0 <pawel.fe@gmail.com>
2023-04-05 13:38:06 +0200

    added multer package to app

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const multer = require("multer");
+const upload = multer();
+
 const app = express();
 
 app.use(cors());
@@ -10,6 +13,10 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", (req, res) => {
+  console.log(req);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
